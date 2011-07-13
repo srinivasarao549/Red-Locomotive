00f6a246967aa98c89e6bfa36901cfdd9379358b
Robert Hurst <robert@thinktankdesign.ca>
2011-07-12 23:39:37 -0700

    Updated the example.

--- examples/basic/example.js
+++ examples/basic/example.js
@@ -1,12 +1,14 @@
 jQuery(function () {
     "use strict"
     RedLocomotive({
-        "baseUrl": "../../"
+        "baseUrl": "../../",
+		"fps": 60,
+		"showFPS": true
     }, function(engine) {
 
 		window.engine = engine;
 
-		engine.viewport.create('main', 'canvas', 1024, 768);
+		var mainView = engine.viewport.create('main', 'canvas', 800, 600);
 
 		//create a test sprite sheet
         engine.spriteSheet.create([
@@ -19,7 +21,27 @@ jQuery(function () {
 
         	//create a test element
         	var testElement = engine.element.create('test', 'sprites/test.png', 100, 100, 100, 100);
-			
+
+			var testTextElement = engine.text.create('test', 'test element', 48, 10, 100);
+
+			engine.every(function () {
+
+				if ((testTextElement.x + 400) < mainView.node[0].width) {
+
+					testTextElement.x = testTextElement.x + 1;
+
+				} else {
+					testTextElement.x = 0;
+				}
+
+			});
+
+			engine.after(function () {
+
+				testElement.y = 200;
+
+			}, 100);
+
     	});
     });
 });
