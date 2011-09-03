378b70f0067f0697e8cb8ff88e354cf056f14328
Robert Hurst <robert@thinktankdesign.ca>
2011-09-03 00:53:54 -0700

    Summary: Incomplete Commit! Started the collisions demo.

--- examples/collisions/example.js
+++ examples/collisions/example.js
@@ -3,17 +3,23 @@ RedLocomotive({
 	//Configuration
 	"baseUrl": "../../",
 	"spriteSheets": [
-		["test", "sprites/test.png", 100, 100]
+		["box", "sprites/box.png", 100, 100]
 	]
 
 }, function(engine) {
 	engine.start();
 
-
 	//Create the main viewport
 	var mainView = engine.viewport.create('main', 'canvas', 800, 600);
 
+	//create a box
+	var box = engine.element.create('box', 'test', 100, 100, 1);
+	engine.animate.sequence(box, [[0, 0]]);
 
-	alert('done');
+	//create a number of random circles to
+	for (var i = 0; i < 100; i += 1) {
+		var circle = engine.element.create('circle' + engine.idGen(), 'test', engine.random(500), engine.random(350), 1);
+		engine.animate.sequence(circle, [[0, 0]]);
+	}
 
 });
